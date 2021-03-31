4ca75f2d7d222a2f4a5709034eecfc8e14af8a3d
brigitap <brigita.perkovic@vuv.hr>
2021-03-31 15:07:26 +0200

    Moj treci commit

--- lv1_github/js/app.js
+++ lv1_github/js/app.js
@@ -5,12 +5,17 @@ $(function()
         var godina = $('#godina').val();
         var bodovi = $('#bodovi').val();
 
-        var podatci = '<tr><td>'+predmet+'</td><td>'+godina+'</td> <td>'+bodovi+'</td><td>'+'<button class="obrisi">Obriši</button>'+'</td</tr>';
+        var podatci = '<tr><td>'+predmet+'</td><td>'+godina+'</td> <td>'+bodovi+'</td><td>'+'<button class="obrisi">Obriši</button>'+'</td>'+'<td><input type="checkbox" id="polozeno">'+'</td></tr>';
         $('tbody').append(podatci);
 
     });
+      
     $(document).on('click', '.obrisi',function(){
         $(this).closest('tr').remove();
+    });
+   
+    $(document).on('click', '#polozeno',function(){
+        $(this).css('background', 'blue');
     });
 });
 
