222e6260fa9f787a9378dfb40b0dc223557aba7b
brigitap <brigita.perkovic@vuv.hr>
2021-03-31 14:29:07 +0200

    Moj drugi commit

--- lv1_github/js/app.js
+++ lv1_github/js/app.js
@@ -5,9 +5,12 @@ $(function()
         var godina = $('#godina').val();
         var bodovi = $('#bodovi').val();
 
-        var podatci = '<tr><td>'+predmet+'</td><td>'+godina+'</td> <td>'+bodovi+'</td></tr>';
+        var podatci = '<tr><td>'+predmet+'</td><td>'+godina+'</td> <td>'+bodovi+'</td><td>'+'<button class="obrisi">Obriši</button>'+'</td</tr>';
         $('tbody').append(podatci);
 
+    });
+    $(document).on('click', '.obrisi',function(){
+        $(this).closest('tr').remove();
     });
 });
 
